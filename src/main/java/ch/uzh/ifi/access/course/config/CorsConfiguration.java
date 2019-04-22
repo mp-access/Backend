c36c8a08b9d7b2fd68ee5717f200d208d4f64ef8
Alexander Hofmann <alexhofmann@gmail.com>
2019-04-22 19:43:22 +0200

    Add configs for cors in case security is disabled

---
+++ src/main/java/ch/uzh/ifi/access/course/config/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package ch.uzh.ifi.access.course.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ConditionalOnProperty(prefix = "rest.security", value = "enabled", havingValue = "false")
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+}
