b871623cafa79775bd525a87fb514da9702b13ea
Alexander Hofmann <alexhofmann@gmail.com>
2019-04-22 18:37:57 +0200

    Move keycloak stuff to separate repository
    It was getting confusing...

--- clear-docker-volumes.sh
+++
@@ -1,9 +0,0 @@
-if [ "$1" = "docker" ]; then
-    echo "Removing data from docker volume"
-    docker rm $(docker ps -a -q) -f
-    docker volume prune
-elif [ "$1" = "standalone" ]; then
-    echo "Removing data from standalone"
-    rm -rf "$2/standalone/data"
-    rm -rf "$2/standalone/tmp"
-fi
