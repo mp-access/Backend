4262d4eeda4d5320c7a67585767f2accccdc2439
Alexander Hofmann <alexhofmann@gmail.com>
2019-05-11 01:24:25 +0200

    Set servlet context path

--- src/main/java/ch/uzh/ifi/access/course/api/CourseResource.java
+++ src/main/java/ch/uzh/ifi/access/course/api/CourseResource.java
@@ -13,7 +13,7 @@ import java.util.Date;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/courses")
+@RequestMapping("/courses")
 public class CourseResource {
 
     @RequestMapping("/")
--- src/main/java/ch/uzh/ifi/access/course/api/UserResource.java
+++ src/main/java/ch/uzh/ifi/access/course/api/UserResource.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/users")
 public class UserResource {
 
     private static final Logger logger = LoggerFactory.getLogger(UserResource.class);
