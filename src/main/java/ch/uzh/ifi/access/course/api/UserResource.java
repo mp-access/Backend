d7556732e8b43636d25487be9979823265682666
Alexander Hofmann <alexhofmann@gmail.com>
2019-05-11 01:51:22 +0200

    Change /users/courses to return list of courses for user

--- src/main/java/ch/uzh/ifi/access/course/api/UserResource.java
+++ src/main/java/ch/uzh/ifi/access/course/api/UserResource.java
@@ -1,20 +1,17 @@
 package ch.uzh.ifi.access.course.api;
 
+import ch.uzh.ifi.access.course.Model.security.GrantedCourseAccess;
+import ch.uzh.ifi.access.course.config.CourseAuthentication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.Serializable;
 import java.security.Principal;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
@@ -23,28 +20,17 @@ public class UserResource {
     private static final Logger logger = LoggerFactory.getLogger(UserResource.class);
 
     @GetMapping
-    @PreAuthorize("hasAnyAuthority('student')")
     public ResponseEntity<Principal> getUser(Principal principal) {
         return ResponseEntity.ok(principal);
     }
 
     @GetMapping(path = "/admin")
-    @PreAuthorize("hasAnyAuthority('author')")
     public ResponseEntity<String> getAdmin(Principal principal) {
         return ResponseEntity.ok("ggwp admin " + principal.getName());
     }
 
-    @GetMapping("/demo")
-    public ResponseEntity<List<String>> demo() {
-        return ResponseEntity.ok(List.of("Alice", "Bob", "Ollie"));
-    }
-
     @GetMapping("/courses")
-    public Object getCourses(OAuth2Authentication principal) {
-        Map<String, Serializable> extensions = principal.getOAuth2Request().getExtensions();
-        Set courses = (HashSet) extensions.get("courses");
-
-        logger.info("Fetched courses: " + courses.toString());
-        return courses;
+    public List<String> getCourses(CourseAuthentication authentication) {
+        return authentication.getCourseAccesses().stream().map(GrantedCourseAccess::getCourse).collect(Collectors.toList());
     }
 }
