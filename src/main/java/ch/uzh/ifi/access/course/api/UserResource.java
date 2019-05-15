c4415d3930dfd40575d72fbfc92295fdd42ebfda
Alexander Hofmann <alexhofmann@gmail.com>
2019-05-15 19:07:46 +0200

    Fix failing tests

--- src/main/java/ch/uzh/ifi/access/course/api/UserResource.java
+++ src/main/java/ch/uzh/ifi/access/course/api/UserResource.java
@@ -2,22 +2,17 @@ package ch.uzh.ifi.access.course.api;
 
 import ch.uzh.ifi.access.course.Model.security.GrantedCourseAccess;
 import ch.uzh.ifi.access.course.config.CourseAuthentication;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
-
-    private static final Logger logger = LoggerFactory.getLogger(UserResource.class);
 
     @GetMapping
     public ResponseEntity<Principal> getUser(Principal principal) {
@@ -30,7 +25,7 @@ public class UserResource {
     }
 
     @GetMapping("/courses")
-    public List<String> getCourses(CourseAuthentication authentication) {
-        return authentication.getCourseAccesses().stream().map(GrantedCourseAccess::getCourse).collect(Collectors.toList());
+    public Set<GrantedCourseAccess> getCourses(CourseAuthentication authentication) {
+        return authentication.getCourseAccesses();
     }
 }
--- src/main/java/ch/uzh/ifi/access/course/config/CourseServiceSetup.java
+++ src/main/java/ch/uzh/ifi/access/course/config/CourseServiceSetup.java
@@ -1,17 +1,19 @@
 package ch.uzh.ifi.access.course.config;
 
-import ch.uzh.ifi.access.course.service.CourseService;
+import ch.uzh.ifi.access.course.Model.Course;
+import ch.uzh.ifi.access.course.dao.CourseDAO;
 import ch.uzh.ifi.access.course.keycloak.KeycloakClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.UUID;
+import java.util.List;
 
 @Configuration
 @ConditionalOnProperty(prefix = "course.users", value = "init-on-startup", havingValue = "true")
 public class CourseServiceSetup {
 
-    public CourseServiceSetup(KeycloakClient keycloakClient, CourseService courseRepository) {
-        keycloakClient.enrollUsersInCourse(courseRepository.getCourseById(UUID.randomUUID()).orElse(null));
+    public CourseServiceSetup(KeycloakClient keycloakClient, CourseDAO courseRepository) {
+        List<Course> courses = courseRepository.selectAllCourses();
+        courses.forEach(keycloakClient::enrollUsersInCourse);
     }
 }
