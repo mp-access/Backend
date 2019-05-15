dce7a85c3dbb0ababd8a150dcb7e404f81899dd2
soyabeen <sein.anschluss@gmail.com>
2019-05-15 15:13:09 +0200

    Add data annotation to courseDTO. Rem. static factory method.

--- src/main/java/ch/uzh/ifi/access/course/dto/CourseDTO.java
+++ src/main/java/ch/uzh/ifi/access/course/dto/CourseDTO.java
@@ -2,13 +2,17 @@ package ch.uzh.ifi.access.course.dto;
 
 import ch.uzh.ifi.access.course.Model.Assignment;
 import ch.uzh.ifi.access.course.Model.Course;
+import lombok.Builder;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+@Data
 public class CourseDTO {
+
     private final UUID id;
 
     private String title;
@@ -16,8 +20,7 @@ public class CourseDTO {
     private String owner;
     private Date startDate;
     private Date endDate;
-
-    private List<UUID> assignmentids = new ArrayList<>();
+    private List<UUID> assignmentids;
 
     public CourseDTO(Course course) {
         this.id = course.getId();
@@ -26,69 +29,16 @@ public class CourseDTO {
         this.owner = course.getOwner();
         this.startDate = course.getStartDate();
         this.endDate = course.getEndDate();
+        this.assignmentids = new ArrayList<>();
 
-        for(Assignment a : course.getAssignments()){
+        for (Assignment a : course.getAssignments()) {
             this.assignmentids.add(a.getId());
         }
     }
 
     public CourseDTO() {
         id = UUID.randomUUID();
+        assignmentids = new ArrayList<>();
     }
 
-    public UUID getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public List<UUID> getAssignmentids() {
-        return assignmentids;
-    }
-
-    public void setAssignmentids(List<UUID> assignmentids) {
-        this.assignmentids = assignmentids;
-    }
-
-    public static CourseDTO setFromModel(Course course) {
-        return new CourseDTO(course);
-    }
 }
