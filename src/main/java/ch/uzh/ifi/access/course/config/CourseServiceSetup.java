cb476d37903a64968fcf95e904a5a1b8840421df
Alexander Hofmann <alexhofmann@gmail.com>
2019-05-27 13:54:28 +0200

    Do not crash if keycloak is not reachable at startup
    Add springretry to handle retries and logging of attempts

--- src/main/java/ch/uzh/ifi/access/course/config/CourseServiceSetup.java
+++ src/main/java/ch/uzh/ifi/access/course/config/CourseServiceSetup.java
@@ -1,10 +1,17 @@
 package ch.uzh.ifi.access.course.config;
 
-import ch.uzh.ifi.access.course.model.Course;
 import ch.uzh.ifi.access.course.dao.CourseDAO;
+import ch.uzh.ifi.access.course.model.Course;
 import ch.uzh.ifi.access.keycloak.KeycloakClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 
 import java.util.List;
 
@@ -12,8 +19,29 @@ import java.util.List;
 @ConditionalOnProperty(prefix = "course.users", value = "init-on-startup", havingValue = "true")
 public class CourseServiceSetup {
 
+    private static final Logger logger = LoggerFactory.getLogger(CourseServiceSetup.class);
+
+    private KeycloakClient keycloakClient;
+
+    private CourseDAO courseRepository;
+
     public CourseServiceSetup(KeycloakClient keycloakClient, CourseDAO courseRepository) {
+        this.keycloakClient = keycloakClient;
+        this.courseRepository = courseRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    @Retryable(
+            backoff = @Backoff(delay = 5000)
+    )
+    public void initializedCourseParticipants() {
         List<Course> courses = courseRepository.selectAllCourses();
         courses.forEach(keycloakClient::enrollUsersInCourse);
+    }
+
+    @SuppressWarnings("unused")
+    @Recover
+    void logFailedAttemptToInitializeParticipants() {
+        logger.warn("Failed to initialize participants: could not connect to identity provider");
     }
 }
---
+++ src/main/java/ch/uzh/ifi/access/course/config/RetryConfig.java
@@ -0,0 +1,30 @@
+package ch.uzh.ifi.access.course.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.RetryCallback;
+import org.springframework.retry.RetryContext;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.listener.RetryListenerSupport;
+
+@EnableRetry
+@Configuration
+public class RetryConfig {
+    private static final Logger logger = LoggerFactory.getLogger(RetryConfig.class);
+
+    @Bean
+    public RetryListenerSupport retryListenerSupport() {
+        return new RetryListenerSupport() {
+            @Override
+            public <T, E extends Throwable> void onError(
+                    RetryContext context, RetryCallback<T, E> callback, Throwable throwable) {
+                logger.warn("Retryable method {} threw {}th exception {}",
+                        context.getAttribute("context.name"),
+                        context.getRetryCount(), throwable.toString());
+                super.onError(context, callback, throwable);
+            }
+        };
+    }
+}
