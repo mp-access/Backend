092f8fac087ca13700ac44b0dccd9177f056570b
soyabeen <sein.anschluss@gmail.com>
2019-06-12 23:40:52 +0200

    Fix codacy review suggestions #59.
    - combine if statements
    - rem unused imports
    - rename method

--- src/main/java/ch/uzh/ifi/access/course/model/Exercise.java
+++ src/main/java/ch/uzh/ifi/access/course/model/Exercise.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
