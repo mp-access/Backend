5804ddc734afe635d8a702e78d2dc33036bf5881
soyabeen <sein.anschluss@gmail.com>
2019-06-13 05:27:21 +0200

    Addjust to new processor way #59.

--- src/test/java/ch/uzh/ifi/access/student/evaluation/EvalMachineBuilderTest.java
+++ src/test/java/ch/uzh/ifi/access/student/evaluation/EvalMachineBuilderTest.java
@@ -1,51 +1,30 @@
 package ch.uzh.ifi.access.student.evaluation;
 
-import ch.uzh.ifi.access.course.model.Exercise;
-import ch.uzh.ifi.access.course.model.ExerciseType;
-import ch.uzh.ifi.access.student.model.TextSubmission;
+import ch.uzh.ifi.access.student.evaluation.process.EvalMachineFactory;
+import ch.uzh.ifi.access.student.evaluation.process.step.DelegateCodeExecStep;
+import ch.uzh.ifi.access.student.evaluation.process.step.GradeSubmissionStep;
+import ch.uzh.ifi.access.student.evaluation.process.step.RouteSubmissionStep;
 import org.assertj.core.api.Assertions;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.statemachine.StateMachine;
-import org.springframework.statemachine.config.StateMachineBuilder;
-
-import java.util.EnumSet;
 
 public class EvalMachineBuilderTest {
 
-    private EvalActionHandler actionHandler;
+    private String submissionId = "sub1";
 
     private StateMachine stateMachine;
 
     @Before
     public void setUp() throws Exception {
-
-        actionHandler = new EvalActionHandler();
-
-        StateMachineBuilder.Builder<EvalMachine.States, EvalMachine.Events> builder
-                = StateMachineBuilder.builder();
-
-        builder.configureStates().withStates()
-                .initial(EvalMachine.States.SUBMITTED)
-                .end(EvalMachine.States.GRADED)
-                .states(EnumSet.allOf(EvalMachine.States.class));
-
-        builder.configureTransitions()
-                .withExternal()
-                .source(EvalMachine.States.SUBMITTED).target(EvalMachine.States.GRADED)
-                .event(EvalMachine.Events.GRADE)
-                .action(actionHandler.grade())
-                .and()
-                .withExternal()
-                .source(EvalMachine.States.SUBMITTED).target(EvalMachine.States.DELEGATED)
-                .event(EvalMachine.Events.DELEGATE)
-                .and()
-                .withExternal()
-                .source(EvalMachine.States.DELEGATED).target(EvalMachine.States.RETURNED)
-                .event(EvalMachine.Events.RETURN);
-
-        stateMachine = builder.build();
+        stateMachine = EvalMachineFactory.initSMForSubmission(submissionId);
         stateMachine.start();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        stateMachine = null;
     }
 
     @Test
@@ -54,6 +33,8 @@ public class EvalMachineBuilderTest {
                 .isEqualTo(EvalMachine.States.SUBMITTED);
 
         Assertions.assertThat(stateMachine).isNotNull();
+        Assertions.assertThat(EvalMachineFactory.extractProcessStep(stateMachine))
+                .isEqualTo(RouteSubmissionStep.class.getName());
     }
 
     @Test
@@ -68,47 +49,43 @@ public class EvalMachineBuilderTest {
     public void grade() {
         stateMachine.sendEvent(EvalMachine.Events.GRADE);
 
-        Assertions.assertThat(stateMachine.getState().getId())
-                .isEqualTo(EvalMachine.States.GRADED);
+        Assertions.assertThat(stateMachine.getState().getId()).isEqualTo(EvalMachine.States.GRADING);
+        Assertions.assertThat(EvalMachineFactory.extractProcessStep(stateMachine))
+                .isEqualTo(GradeSubmissionStep.class.getName());
     }
 
+    @Test
+    public void delegate() {
+        stateMachine.sendEvent(EvalMachine.Events.DELEGATE);
+
+        Assertions.assertThat(stateMachine.getState().getId()).isEqualTo(EvalMachine.States.DELEGATE);
+        Assertions.assertThat(EvalMachineFactory.extractProcessStep(stateMachine))
+                .isEqualTo(DelegateCodeExecStep.class.getName());
+    }
+
+    @Test
+    public void returning() {
+        stateMachine.sendEvent(EvalMachine.Events.DELEGATE);
+        Assertions.assertThat(stateMachine.getState().getId()).isEqualTo(EvalMachine.States.DELEGATE);
+        Assertions.assertThat(EvalMachineFactory.extractProcessStep(stateMachine))
+                .isEqualTo(DelegateCodeExecStep.class.getName());
+
+
+        stateMachine.sendEvent(EvalMachine.Events.RETURN);
+        Assertions.assertThat(stateMachine.getState().getId()).isEqualTo(EvalMachine.States.RETURNING);
+//        Assertions.assertThat(EvalMachineFactory.extractProcessStep(stateMachine))
+//                .isEqualTo(DelegateCodeExecStep.class.getName());
+    }
 
     @Test
     public void ignoreWrongEvent() {
         stateMachine.sendEvent(EvalMachine.Events.DELEGATE);
         Assertions.assertThat(stateMachine.getState().getId())
-                .isEqualTo(EvalMachine.States.DELEGATED);
+                .isEqualTo(EvalMachine.States.DELEGATE);
 
         stateMachine.sendEvent(EvalMachine.Events.GRADE);
         Assertions.assertThat(stateMachine.getState().getId())
-                .isEqualTo(EvalMachine.States.DELEGATED);
-    }
-
-    @Test
-    public void gradeTextSubmission() {
-        Exercise ex = Exercise.builder()
-                .id("e1")
-                .type(ExerciseType.text)
-                .build();
-
-        TextSubmission sub = TextSubmission.builder()
-                .id("1")
-                .answer("something")
-                .exerciseId(ex.getId())
-                .build();
-
-        actionHandler.storeSubmission(sub);
-        actionHandler.storeExercise(ex);
-
-        stateMachine.getExtendedState().getVariables().put("id", sub.getId());
-
-        stateMachine.sendEvent(EvalMachine.Events.GRADE);
-
-        Assertions.assertThat(stateMachine.getState().getId())
-                .isEqualTo(EvalMachine.States.GRADED);
-
-        Assertions.assertThat(actionHandler.getSubmission(sub.getId()).getResult().getScore())
-                .isEqualTo(0);
+                .isEqualTo(EvalMachine.States.DELEGATE);
     }
 
 }
