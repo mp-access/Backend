0d27ed6db9e2ced97b33f4ee48e56db02cc29b4c
soyabeen <sein.anschluss@gmail.com>
2019-06-13 05:26:13 +0200

    Add eval machine factory and new state names #59.

--- src/main/java/ch/uzh/ifi/access/student/evaluation/EvalMachine.java
+++ src/main/java/ch/uzh/ifi/access/student/evaluation/EvalMachine.java
@@ -3,11 +3,11 @@ package ch.uzh.ifi.access.student.evaluation;
 public class EvalMachine {
 
     public enum States {
-        SUBMITTED, DELEGATED, RETURNED, GRADED
+        SUBMITTED, DELEGATE, RETURNING, GRADING, FINISHED
     }
 
     public enum Events {
-        GRADE, DELEGATE, RETURN
+        GRADE, DELEGATE, RETURN, FINISH
     }
 
 }
