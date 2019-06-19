caa43020122e1b8e71ac1988ba236be70f5a01ff
soyabeen <sein.anschluss@gmail.com>
2019-06-19 15:04:22 +0200

    Log route submission step #59.

--- src/main/java/ch/uzh/ifi/access/student/evaluation/process/step/RouteSubmissionStep.java
+++ src/main/java/ch/uzh/ifi/access/student/evaluation/process/step/RouteSubmissionStep.java
@@ -1,14 +1,18 @@
 package ch.uzh.ifi.access.student.evaluation.process.step;
 
-import ch.uzh.ifi.access.student.evaluation.EvalMachine;
+import ch.uzh.ifi.access.student.evaluation.process.EvalMachine;
 import ch.uzh.ifi.access.student.model.CodeSubmission;
 import ch.uzh.ifi.access.student.model.StudentSubmission;
 import ch.uzh.ifi.access.student.service.StudentSubmissionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
 public class RouteSubmissionStep implements ProcessStep {
+
+    private static final Logger logger = LoggerFactory.getLogger(RouteSubmissionStep.class);
 
     private StudentSubmissionService submissionService;
 
@@ -26,6 +30,7 @@ public class RouteSubmissionStep implements ProcessStep {
             resultingEvent = EvalMachine.Events.DELEGATE;
         }
 
+        logger.debug(String.format("Route submission %s to %s.", submissionId, resultingEvent));
         return resultingEvent;
     }
 
