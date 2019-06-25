3aa006a030f5c0f536b5cf661b6a439a20cb0ded
soyabeen <sein.anschluss@gmail.com>
2019-06-25 14:46:37 +0200

    Add @service annotation to submission code runner #59.

--- src/main/java/ch/uzh/ifi/access/student/evaluation/runner/SubmissionCodeRunner.java
+++ src/main/java/ch/uzh/ifi/access/student/evaluation/runner/SubmissionCodeRunner.java
@@ -10,6 +10,7 @@ import com.spotify.docker.client.exceptions.DockerException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
@@ -18,6 +19,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+@Service
 public class SubmissionCodeRunner {
 
     private static final Logger logger = LoggerFactory.getLogger(SubmissionCodeRunner.class);
