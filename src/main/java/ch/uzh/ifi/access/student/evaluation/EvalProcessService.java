3a075eacfa4aae8a30dcaa370e4cf5a13c5ee47e
soyabeen <sein.anschluss@gmail.com>
2019-06-25 14:45:45 +0200

    Add async fireEvalProcessExecutionAsync #59.

--- src/main/java/ch/uzh/ifi/access/student/evaluation/EvalProcessService.java
+++ src/main/java/ch/uzh/ifi/access/student/evaluation/EvalProcessService.java
@@ -9,10 +9,12 @@ import ch.uzh.ifi.access.student.model.StudentSubmission;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class EvalProcessService {
@@ -29,7 +31,7 @@ public class EvalProcessService {
         this.machineRepo = machineRepo;
     }
 
-    public String initEvalProcess(StudentSubmission submission){
+    public String initEvalProcess(StudentSubmission submission) {
         String processId = UUID.randomUUID().toString();
         StateMachine m = null;
         try {
@@ -37,30 +39,51 @@ public class EvalProcessService {
             m.start();
             machineRepo.store(processId, m);
         } catch (Exception e) {
-            logger.error("Could not create state machine for submission: "+ submission.getId());
+            logger.error("Could not create state machine for submission: " + submission.getId());
             logger.error(e.getMessage());
         }
 
         return processId;
     }
 
-    public void processSubmissionStep(String submissionId){
+    @Async
+    public void fireEvalProcessExecutionAsync(final String processId) {
+        StateMachine machine = machineRepo.get(processId);
+        try {
 
-        StateMachine machine = machineRepo.get(submissionId);
+            while (EvalMachine.States.FINISHED != machine.getState().getId()) {
 
-        if(EvalMachine.States.FINISHED != machine.getState().getId()) {
-            String step = EvalMachineFactory.extractProcessStep(machine);
-            logger.debug("current step: " + step);
-            EvalMachine.Events nextEvent = stepFactory.getStep(step).execute(submissionId);
-            machine.sendEvent(nextEvent);
-            machineRepo.store(submissionId, machine);
-        }else{
-            logger.debug("Submission with id "+ submissionId +" is finished.");
+                machine = executeStep(machine);
+                machineRepo.store(processId, machine);
+
+            }
+            logger.debug("Submission with id " + processId + " is finished.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
     }
 
-    private ProcessStep getStepForMachine(){
-        return null;
+    private StateMachine executeStep(StateMachine machine) throws InterruptedException {
+        String step = EvalMachineFactory.extractProcessStep(machine);
+        int stepDelayInS = EvalMachineFactory.extractProcessStepDelayInS(machine);
+        String submissionId = EvalMachineFactory.extractSubmissionId(machine);
+
+        logger.debug(String.format("Current submission %s in state %s", submissionId, machine.getState().getId()));
+        logger.debug(String.format("Execute step %s with delay of %d", step, stepDelayInS));
+
+        if (stepDelayInS > 0) {
+            logger.debug("Delaying ...");
+            TimeUnit.SECONDS.sleep(Long.parseLong(stepDelayInS + ""));
+        }
+
+        ProcessStep stepObj = stepFactory.getStep(step);
+        if(stepObj  != null){
+            EvalMachine.Events nextEvent = stepObj.execute(submissionId);
+            machine.sendEvent(nextEvent);
+        }
+
+        return machine;
     }
+
 }
