a4c35d9e7f0225c6745d4a47191f0d2c39dd04f1
soyabeen <sein.anschluss@gmail.com>
2019-08-14 02:43:13 +0200

    Adjust to new submission eval obj #116.

--- src/main/java/ch/uzh/ifi/access/student/model/SubmissionEvaluation.java
+++ src/main/java/ch/uzh/ifi/access/student/model/SubmissionEvaluation.java
@@ -1,15 +1,19 @@
 package ch.uzh.ifi.access.student.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
 import lombok.Value;
 
 import java.time.Instant;
 
 @SuppressWarnings("unused")
 @Value
+@Data
+@Builder
 public class SubmissionEvaluation {
 
-    public static SubmissionEvaluation NO_SUBMISSION = new SubmissionEvaluation(0, Instant.MIN);
+    public static SubmissionEvaluation NO_SUBMISSION = new SubmissionEvaluation(0, 0, 0, Instant.MIN);
 
     private int correctPoints;
 
