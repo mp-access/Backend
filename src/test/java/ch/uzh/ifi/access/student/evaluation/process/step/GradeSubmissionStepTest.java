aebfe08136cd3957b705216b679a16e3854b80d8
Alexander Hofmann <alexhofmann@gmail.com>
2019-08-17 20:59:57 +0200

    Refactor grade submission step

---
+++ src/test/java/ch/uzh/ifi/access/student/evaluation/process/step/GradeSubmissionStepTest.java
@@ -0,0 +1,38 @@
+package ch.uzh.ifi.access.student.evaluation.process.step;
+
+import ch.uzh.ifi.access.student.evaluation.evaluator.CodeEvaluator;
+import ch.uzh.ifi.access.student.evaluation.evaluator.MultipleChoiceEvaluator;
+import ch.uzh.ifi.access.student.evaluation.evaluator.StudentSubmissionEvaluator;
+import ch.uzh.ifi.access.student.evaluation.evaluator.TextEvaluator;
+import ch.uzh.ifi.access.student.model.CodeSubmission;
+import ch.uzh.ifi.access.student.model.MultipleChoiceSubmission;
+import ch.uzh.ifi.access.student.model.TextSubmission;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class GradeSubmissionStepTest {
+
+    @Test
+    public void mcEvaluator() {
+        GradeSubmissionStep gradeSubmissionStep = new GradeSubmissionStep(null, null);
+
+        StudentSubmissionEvaluator evaluator = gradeSubmissionStep.evaluator(new MultipleChoiceSubmission());
+        Assertions.assertThat(evaluator).isExactlyInstanceOf(MultipleChoiceEvaluator.class);
+    }
+
+    @Test
+    public void textEvaluator() {
+        GradeSubmissionStep gradeSubmissionStep = new GradeSubmissionStep(null, null);
+
+        StudentSubmissionEvaluator evaluator = gradeSubmissionStep.evaluator(new TextSubmission());
+        Assertions.assertThat(evaluator).isExactlyInstanceOf(TextEvaluator.class);
+    }
+
+    @Test
+    public void codeEvaluator() {
+        GradeSubmissionStep gradeSubmissionStep = new GradeSubmissionStep(null, null);
+
+        StudentSubmissionEvaluator evaluator = gradeSubmissionStep.evaluator(new CodeSubmission());
+        Assertions.assertThat(evaluator).isExactlyInstanceOf(CodeEvaluator.class);
+    }
+}
