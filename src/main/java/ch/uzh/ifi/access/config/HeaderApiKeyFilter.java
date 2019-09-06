e0268ecf8559f7b92f7ad049f7b5187354c55984
Alexander Hofmann <alexhofmann@gmail.com>
2019-09-07 01:38:43 +0200

    Add support for Gitlab webhooks, move webhooks to own controller

--- src/main/java/ch/uzh/ifi/access/config/HeaderApiKeyFilter.java
+++ src/main/java/ch/uzh/ifi/access/config/HeaderApiKeyFilter.java
@@ -16,9 +16,11 @@ import java.io.IOException;
 @Component
 public class HeaderApiKeyFilter extends OncePerRequestFilter {
 
-    private static final String HEADER_NAME = "X-Hub-Signature";
+    private static final String GITHUB_HEADER_NAME = "X-Hub-Signature";
 
-    private static final AntPathRequestMatcher requestMatcher = new AntPathRequestMatcher("/courses/**/update", HttpMethod.POST.toString());
+    private static final String GITLAB_HEADER_NAME = "X-Gitlab-Token";
+
+    private static final AntPathRequestMatcher requestMatcher = new AntPathRequestMatcher("/webhooks/courses/**/update/**", HttpMethod.POST.toString());
 
     private final ApiTokenAuthenticationProvider authenticationProvider;
 
@@ -33,12 +35,16 @@ public class HeaderApiKeyFilter extends OncePerRequestFilter {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (isSupportedPathAndMethod(request)) {
 
-            String githubWebHookHeader = request.getHeader(HEADER_NAME);
-            if (authentication == null && isSupportedHeader(githubWebHookHeader)) {
-                Authentication auth =
-                        authenticationProvider.authenticate(
-                                new ApiTokenAuthenticationProvider.GithubHeaderAuthentication(githubWebHookHeader, null));
-                SecurityContextHolder.getContext().setAuthentication(auth);
+            final String githubWebHookHeader = request.getHeader(GITHUB_HEADER_NAME);
+            final String gitlabWebHookHeader = request.getHeader(GITLAB_HEADER_NAME);
+            boolean isGithub = isSupportedGithubHeader(githubWebHookHeader);
+            boolean isGitlab = isSupportedGitlabHeader(gitlabWebHookHeader);
+
+            if (authentication == null && (isGithub || isGitlab)) {
+                final String principal = isGithub ? githubWebHookHeader : gitlabWebHookHeader;
+                authentication = authenticationProvider.authenticate(
+                        new ApiTokenAuthenticationProvider.HeaderAuthentication(principal, isGithub, isGitlab));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             } else {
                 SecurityContextHolder.clearContext();
             }
@@ -51,7 +57,17 @@ public class HeaderApiKeyFilter extends OncePerRequestFilter {
         return requestMatcher.matches(request);
     }
 
-    private boolean isSupportedHeader(String header) {
+    /**
+     * A github header starts with 'sha1=' to indicate the hashing algorithm
+     */
+    private boolean isSupportedGithubHeader(String header) {
         return header != null && header.startsWith("sha1=");
+    }
+
+    /**
+     * For gitlab it is enough if the header is not null
+     */
+    private boolean isSupportedGitlabHeader(String header) {
+        return header != null;
     }
 }
