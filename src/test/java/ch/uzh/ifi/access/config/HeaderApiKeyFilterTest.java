b37a790d81154e38c8729cbbd5c12db3a011e65b
Alexander Hofmann <alexhofmann@gmail.com>
2019-09-07 11:15:43 +0200

    Fix and add tests

--- src/test/java/ch/uzh/ifi/access/config/HeaderApiKeyFilterTest.java
+++ src/test/java/ch/uzh/ifi/access/config/HeaderApiKeyFilterTest.java
@@ -11,16 +11,29 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.UUID;
 
 public class HeaderApiKeyFilterTest {
+
+    private static final String URL = "/webhooks/courses";
 
     private HeaderApiKeyFilter filter = new HeaderApiKeyFilter(new ApiTokenAuthenticationProvider());
 
     @Test
-    public void doFilterInternal() throws ServletException, IOException {
+    public void otherPathsShouldBeIgnored() throws ServletException, IOException {
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setMethod(HttpMethod.POST.toString());
-        request.setPathInfo("/courses/1/update");
+        request.setPathInfo("/foo/bar");
+        filter.doFilterInternal(request, new MockHttpServletResponse(), new MockFilterChain());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Assertions.assertThat(authentication).isNull();
+    }
+
+    @Test
+    public void githubHeader() throws ServletException, IOException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod(HttpMethod.POST.toString());
+        request.setPathInfo(URL + "/1/update");
 
         final String hash = "sha1=asdf";
         request.addHeader("X-Hub-Signature", hash);
@@ -32,10 +45,25 @@ public class HeaderApiKeyFilterTest {
     }
 
     @Test
+    public void gitlabHeader() throws ServletException, IOException {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod(HttpMethod.POST.toString());
+        request.setPathInfo(URL + "/1/update");
+
+        final String header = UUID.randomUUID().toString();
+        request.addHeader("X-Gitlab-Token", header);
+        filter.doFilterInternal(request, new MockHttpServletResponse(), new MockFilterChain());
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Assertions.assertThat(authentication.getPrincipal()).isEqualTo("Gitlab");
+        Assertions.assertThat(authentication).isInstanceOf(ApiTokenAuthenticationProvider.GitlabHeaderAuthentication.class);
+    }
+
+    @Test
     public void doFilterInternalNoHeaderSet() throws ServletException, IOException {
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setMethod(HttpMethod.POST.toString());
-        request.setPathInfo("/courses/1/update");
+        request.setPathInfo(URL + "/1/update");
 
         filter.doFilterInternal(request, new MockHttpServletResponse(), new MockFilterChain());
 
@@ -43,10 +71,10 @@ public class HeaderApiKeyFilterTest {
     }
 
     @Test
-    public void doFilterInternalWrongHashingAlgorithm() throws ServletException, IOException {
+    public void githubWrongHashingAlgorithm() throws ServletException, IOException {
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setMethod(HttpMethod.POST.toString());
-        request.setPathInfo("/courses/1/update");
+        request.setPathInfo(URL + "/1/update");
         request.addHeader("X-Hub-Signature", "md5=asdf");
 
         filter.doFilterInternal(request, new MockHttpServletResponse(), new MockFilterChain());
