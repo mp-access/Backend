66d4575214c5255639445d3fff60f73620cf9f82
a-a-hofmann <alexhofmann@gmail.com>
2019-09-15 16:57:01 +0200

    Remove unused import

--- src/test/java/ch/uzh/ifi/access/student/controller/SubmissionControllerTest.java
+++ src/test/java/ch/uzh/ifi/access/student/controller/SubmissionControllerTest.java
@@ -17,7 +17,6 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
